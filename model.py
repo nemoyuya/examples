d6c5a4e3267d927064fddea1be1bf0ed4f2b94e8
nemoyuya <100604113+nemoyuya@users.noreply.github.com>
2023-07-28 00:39:15 +0900

    Add files via upload

--- model.py
+++ model.py
@@ -3,40 +3,94 @@ from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import Dataset
 import torch.optim as optim
-
 import numpy as np
+
+from PIL import Image
+from torchvision import transforms
+import cv2
 
 class XAI_image(nn.Module):
     
-    def __init__(self, img_model, cls_num, img_num, batch_size, init_mode, model_load_mode, model_dict=None):
+    def __init__(self, base_img_path, img_model, cls_num, img_num, batch_size, init_mode, model_load_mode, model_dict=None):
         super().__init__()
-
-        self.img_model = img_model
+        self.norm_flag = base_img_path != ""
+        self.mean_1 = 0.485
+        self.mean_2 = 0.456
+        self.mean_3 = 0.406
+        self.std_1 = 0.229
+        self.std_2 = 0.224
+        self.std_3 = 0.225
+        self.preprocess = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[self.mean_1,self.mean_2,self.mean_3], std=[self.std_1,self.std_2,self.std_3]),
+        ])
+        self.img_model = img_model.cuda()
+        self.img_model.eval()
         self.batch_size = batch_size
-        self.matrix_size = 64
-        self.channel_size = 3
         self.img_num = img_num
+        self.matrix_size = 224
+        self.channel_size = 3
         self.emb_size = self.channel_size*self.matrix_size**2
-
         self.emb_img = nn.Embedding(img_num, self.emb_size)
-
-        self.semantics_dim = 200
-
-        self.fc1_for_mode1 = nn.Linear(self.emb_size, self.emb_size, bias = False)
-        self.fcout_mode1 = nn.Linear(self.emb_size, cls_num, bias = False)
         
         # ---------------------------
         # Initialize the internale weight 
         # ---------------------------
         if init_mode == "uniform":
             self.emb_img.weight.data.uniform_(-1, 1)
-
         elif init_mode == "gauss":
-            self.emb_img.weight.data.normal_(0, 0.1)
-            
+            self.emb_img.weight.data.normal_(0, 0.1)            
         else :
             raise NotImplementedError(init_mode)
 
+        
+        if self.norm_flag:
+            #self.eval()
+            input_image = Image.open(base_img_path)
+            self.base_img = transforms.ToTensor()(input_image)
+            self.base_img = transforms.Normalize(mean=[self.mean_1,self.mean_2,self.mean_3], std=[self.std_1,self.std_2,self.std_3])(self.base_img)
+            #self.base_img = self.preprocess(input_image)
+            self.base_img = self.base_img.unsqueeze(0)
+            self.base_img = self.base_img.to('cuda')
+            output_check = self.check(self.base_img)
+            _, predicted = torch.max(output_check, 1)
+            pred_label = predicted[0].cpu().detach().item()
+            print(pred_label, F.softmax(output_check[0],dim=0)[pred_label])
+
+            tmp_img = transforms.Normalize(mean=[-self.mean_1/self.std_1, -self.mean_2/self.std_2, -self.mean_3/self.std_3], std=[1./self.std_1, 1./self.std_2, 1./self.std_3])(self.base_img)
+            #tmp_img = self.base_img
+            np_tmp_img = tmp_img.to('cpu').detach().numpy().copy()
+            np_tmp_img = np_tmp_img[0]
+            np_tmp_img = np_tmp_img.transpose(1,2,0)
+            np_tmp_img *= 255.
+            np_tmp_img = np_tmp_img.astype(np.uint8)
+            np_tmp_img = cv2.cvtColor(np_tmp_img, cv2.COLOR_RGB2BGR)
+            cv2.imwrite(f"np_tmp_img.png",np_tmp_img)
+
+            input_image = Image.open("np_tmp_img.png")
+            test_img = transforms.ToTensor()(input_image)
+            test_img = transforms.Normalize(mean=[self.mean_1,self.mean_2,self.mean_3], std=[self.std_1,self.std_2,self.std_3])(test_img)
+            #test_img = self.preprocess(input_image)
+            test_img = test_img.unsqueeze(0)
+            test_img = test_img.to('cuda')
+            #test_img = test_img.cuda()
+            check = self.check(test_img)
+            check.to('cuda')
+            _, predicted = torch.max(check, 1)
+            pred_label = predicted[0].cpu().detach().item()
+            print(pred_label, F.softmax(check[0],dim=0)[pred_label])
+            
+            for c in range(self.channel_size):
+                for x in range(self.matrix_size):
+                    for y in range(self.matrix_size):
+                        i = c*self.matrix_size*self.matrix_size + y*self.matrix_size + x
+                        self.emb_img.weight.data[0][i] = torch.logit(torch.tensor(tmp_img[0,c,y,x]))
+        else:
+            self.base_img = np.zeros((self.matrix_size,self.matrix_size,3),np.float32)
+            self.base_img = transforms.ToTensor()(self.base_img)
+            self.base_img = self.base_img.unsqueeze(0)
+            self.base_img = self.base_img.to('cuda')
+                       
         self.device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
         if model_load_mode == 1:
@@ -54,11 +108,25 @@ class XAI_image(nn.Module):
     def forward(self, x):
         
         emb = self.emb_img(x)
-        x = emb.reshape(self.img_num, self.channel_size, self.matrix_size, self.matrix_size)
-        x = self.img_model(x)
-        x = F.log_softmax(x, dim=1)
+        emb = torch.sigmoid(emb)
+        emb = emb.reshape(self.img_num, self.channel_size, self.matrix_size, self.matrix_size)
+        if self.norm_flag:
+            norm_emb = transforms.Normalize(mean=[self.mean_1, self.mean_2, self.mean_3], std=[self.std_1, self.std_2, self.std_3])(emb)
+            x = self.img_model(norm_emb)
+        else:
+            x = self.img_model(emb)
+        logsoftmax = F.log_softmax(x, dim=1)
         
-        return x, emb
+        return x, logsoftmax, emb
+
+    
+    def check(self, x):
+        
+        x = self.img_model(x)
+        #x = F.softmax(x, dim=1)
+        #x = F.log_softmax(x, dim=1)
+        
+        return x
 
     
     #def forward(self, x):
--- xai_image.py
+++ xai_image.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import gc
+import cv2
 
 import numpy as np
 import random
@@ -21,15 +22,18 @@ import time
 import pprint
 
 from multiprocessing import Value, Array, Process, Manager 
+from PIL import Image
+from torchvision import transforms
+from torchvision import models
 
 def main(cfg):
 
     # 更新する画像の枚数
-    xai_img_num = 10
+    xai_img_num = 1
 
     # 対象クラスのインデックス
-    cls_index = 0
-    
+    target_cls = 567
+    true_cls = None
     # ----------------------------
     # Model
     # ----------------------------
@@ -39,11 +43,21 @@ def main(cfg):
     else:
         model_dict = None
 
-    resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+    #resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+    #resnet = models.resnet50(pretrained=True)
+    vitb16 = models.vit_b_16(pretrained=True)
+    #exit()
+    
     img_cls_num = 1000
-
+    
+    #base_img_path = ""
+    #base_img_path = "2.jpg"
+    base_img_path = "0_17.png"
+    #base_img_path = ""
+    
     model = XAI_image(
-        img_model = resnet,
+        base_img_path = base_img_path,
+        img_model = vitb16,
         cls_num = img_cls_num,
         img_num = xai_img_num, 
         batch_size = cfg["batch_size"], 
@@ -53,7 +67,7 @@ def main(cfg):
     )
     
     model.to("cuda")
-    #model = torch.nn.DataParallel(model)    
+    #model = torch.nn.DataParallel(model)
     # ----------------------------
     # Optimizer, loss
     # ----------------------------
@@ -77,12 +91,11 @@ def main(cfg):
     best_validation_accuracy = 0
     nan_flag = False
 
-
     train_x = [i for i in range(xai_img_num)]
     train_x = np.array(train_x)
     train_x = torch.tensor(train_x, dtype=torch.int64)
     train_x.to('cuda')
-    train_y = [cls_index for _ in range(xai_img_num)]
+    train_y = [target_cls for _ in range(xai_img_num)]
     train_y = np.array(train_y)
     train_y = torch.tensor(train_y, dtype=torch.int64)
     train_y.to('cuda')
@@ -90,80 +103,111 @@ def main(cfg):
     #print(model.emb_img(train_x).cpu().detach().item())
     #exit()
     
+    model.eval()
+
+    if model.norm_flag:
+        output_check = model.check(model.base_img)
+        _, predicted = torch.max(output_check, 1)
+        pred_label = predicted[0].cpu().detach().item()
+        print(pred_label, F.softmax(output_check[0],dim=0)[pred_label])
+        true_cls = pred_label
+        #print(pred_label, output_check[0][pred_label])
+        #exit()
+    
     for epoch in tqdm(range(cfg["epoch_num"])):
         start = time.time()
-        with open("log.csv","a") as f:
-            print("Epoch >>>>> ", epoch + epoch_offset)
+        print("Epoch >>>>> ", epoch + epoch_offset)
+        
+        #model.train()
+        train_loss = 0.
+        train_accuracy = 0
+        train_total = 0
+
+        opt.zero_grad()
+
+        train_pred_cnt = [0 for i in range(img_cls_num)]
+        each_label_accuracy = [0 for i in range(img_cls_num)]
+        train_x = train_x.cuda()
+        train_y = train_y.cuda()
+        output_train_x, logsoftmax, emb_img = model(train_x)
+        output_train_x.to('cuda')
+        logsoftmax.to('cuda')
+        loss = nn.functional.nll_loss(logsoftmax, train_y)
+
+        for i in range(xai_img_num):
+            np_img = np.zeros((model.matrix_size,model.matrix_size,model.channel_size), np.float64)
+            np_emb_img = emb_img[i]
+            for c in range(model.channel_size):
+                for x in range(model.matrix_size):
+                    for y in range(model.matrix_size):
+                        #ii = c*model.matrix_size*model.matrix_size + x*model.matrix_size + y
+                        np_img[y,x,c] = np_emb_img[c,y,x]
+            np_img = 255*np_img
+            print(np.min(np_img),np.max(np_img))
+            np_img = np_img.astype(np.uint8)
+            np_img = cv2.cvtColor(np_img, cv2.COLOR_RGB2BGR)
+            save_path = f"{i}_{epoch}.png"
+            cv2.imwrite(save_path, np_img)
+            print(f"saved as {save_path}")
             
-            model.train()
-            train_loss = 0.
-            train_accuracy = 0
-            train_total = 0
-    
-            opt.zero_grad()
+        loss.backward()
+        
+        emb_grad = model.emb_img.weight.grad
+        emb_grad_absmean = torch.mean(torch.absolute(emb_grad))
+        #update_coef = 1./5#1./255.
+        update_coef = 1./255.
+        print("emb_grad_absmean",emb_grad_absmean)
+        p_emb_grad_thresh = torch.nn.Threshold(emb_grad_absmean,0.)(emb_grad)
+        p_emb_grad_sign = torch.sign(p_emb_grad_thresh)
+        n_emb_grad_thresh = torch.nn.Threshold(emb_grad_absmean,0.)(-emb_grad)
+        n_emb_grad_sign = torch.sign(n_emb_grad_thresh)
+        for i in range(xai_img_num):
+            for j in range(model.channel_size*model.matrix_size**2):
+                model.emb_img.weight.data[i][j] = model.emb_img.weight.data[i][j] - update_coef*(p_emb_grad_sign[i][j] - n_emb_grad_sign[i][j])
 
-            train_pred_cnt = [0 for i in range(img_cls_num)]
-            each_label_accuracy = [0 for i in range(img_cls_num)]
-            train_x = train_x.cuda()
-            train_y = train_y.cuda()
-            output_train_x, emb_img = model(train_x)
-            output_train_x.to('cuda')
-            loss = nn.functional.nll_loss(output_train_x, train_y)
+        #opt.step()
+                
+        train_loss += loss.item()
+        
+        model.eval()
+        
+        _, predicted = torch.max(logsoftmax, 1)
+        for i,true_label_tensor in enumerate(train_y):
+            true_label = true_label_tensor.cpu().detach().item()
+            pred_label = predicted[i].cpu().detach().item()
+        train_total += train_y.size(0)
+        
+        if np.isnan(train_loss):
+            print("nan")
+            exit()
 
-            for i in range(xai_img_num):
-                np_img = np.zeros((model.matrix_size,model.matrix_size,model.channel_size), np.float32)
-                np_emb_img = emb_img[i]
-                for c in range(model.channel_size):
-                    for x in range(model.matrix_size):
-                        for y in range(model.matrix_size):
-                            ii = c*model.matrix_size*model.matrix_size + x*model.matrix_size + y
-                            np_img[y,x,c] = np_emb_img[ii]
-                print(np_img)
+        if model.norm_flag:
+            print(true_cls, target_cls, pred_label, F.softmax(output_train_x)[0][true_cls], F.softmax(output_train_x)[0][target_cls])
+        else:
+            print(target_cls, pred_label, F.softmax(output_train_x)[0][target_cls])
             
-            loss.backward()
-            opt.step()
-                    
-            train_loss += loss.item()
+        ###
+        # GPUの解放
+        ###
+        del loss
+        gc.collect()
+        torch.cuda.empty_cache() # <-
+                
+        #print("mode1_each", each_label_accuracy, np.sum(np.array(each_label_accuracy)) )
 
-            _, predicted = torch.max(output_train_x, 1)
-            for i,true_label_tensor in enumerate(train_y):
-                true_label = true_label_tensor.cpu().detach().item()
-                pred_label = predicted[i].cpu().detach().item()
-                each_label_accuracy[true_label] += (true_label == pred_label)
-                train_pred_cnt[pred_label] += 1
-            train_accuracy += each_label_accuracy[cls_index]
-            train_total += train_y.size(0)
-            
-            if np.isnan(train_loss):
-                print("nan")
-                exit()
-            
-            ###
-            # GPUの解放
-            ###
-            del loss
-            gc.collect()
-            torch.cuda.empty_cache() # <-
-                    
-            #print("mode1_each", each_label_accuracy, np.sum(np.array(each_label_accuracy)) )
-    
-            t_loss = (train_loss / train_total)
-            t_accuracy = (100 * train_accuracy / train_total)
-            print("train (loss, accuracy)",t_loss,t_accuracy)
-            writer.add_scalar("loss/train", t_loss, epoch + epoch_offset)
-            writer.add_scalar("accuracy/train", t_accuracy, epoch + epoch_offset)
+        t_loss = (train_loss / train_total)
+        print("train (loss)",t_loss)
 
-            if t_loss < best_train_loss:
-                best_train_loss = t_loss
-                # ----------------------------
-                # save model
-                # ----------------------------
-                print(f"model saved as model_best_train.pth")
-                torch.save(model.state_dict(), "model_best_train.pth")
-            torch.save(model.state_dict(), "model_latest.pth")
+        if t_loss < best_train_loss:
+            best_train_loss = t_loss
+            # ----------------------------
+            # save model
+            # ----------------------------
+            print(f"model saved as model_best_train.pth")
+            torch.save(model.state_dict(), "model_best_train.pth")
+        torch.save(model.state_dict(), "model_latest.pth")
 
-            elapsed_time = time.time() - start
-            f.write(f"{epoch + epoch_offset},{t_loss},{t_accuracy},{elapsed_time}\n")
+        elapsed_time = time.time() - start
     writer.close()
 
 
@@ -172,4 +216,4 @@ if __name__ == "__main__":
 
     with open(os.path.join('conf','parameters.yaml'),'r') as yml:
         cfg = yaml.safe_load(yml)
-        model, train_loader, pre = main(cfg)
+        main(cfg)
